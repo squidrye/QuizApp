21c6af0d377bd4ddc901de7346626e7d62e9a0e2
Apophis_ <ancientphoebus@gmail.com>
2021-06-18 14:20:03 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+void main(){
+runApp(new FirstApp());
+}
+class FirstApp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(home: Text('Hello World!'));
+  }
+
+}
